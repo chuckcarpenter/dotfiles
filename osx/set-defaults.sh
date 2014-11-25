f892c379cf7d740e9dccb52aed8a791ede7ab4a2
Chuck Carpenter <chuck.carpenter@me.com>
2014-11-25 17:06:19 -0500

    setting some of my own defaults

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -v
 
 # ~/.osx — http://mths.be/osx
 
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,68 @@
 alias reload!='. ~/.zshrc'
+
+# -- Extras --------------------------------------------------------------------
+
+# Server
+alias server='open http://localhost:8000 && http-server -p 8000'
+
+# be nice
+alias please=sudo
+alias hosts='sudo $EDITOR /etc/hosts'   # yes I occasionally 127.0.0.1 twitter.com ;)
+
+# handy things
+alias bunyip='node ~/code/bunyip/cli.js'
+alias spotify="osascript ~/code/SpotifyControl/SpotifyControl.scpt"
+alias vlc="osascript ~/code/VLCControl/VLCControl.scpt"
+
+# GIT STUFF
+
+# Undo a `git push`
+alias undopush="git push -f origin HEAD^:master"
+
+# git root
+alias gr='[ ! -z `git rev-parse --show-cdup` ] && cd `git rev-parse --show-cdup || pwd`'
+
+# IP addresses
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en1"
+alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
+
+# Enhanced WHOIS lookups
+alias whois="whois -h whois-servers.net"
+
+# Flush Directory Service cache
+alias flush="dscacheutil -flushcache"
+
+# View HTTP traffic
+alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
+alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
+
+# Recursively delete `.DS_Store` files
+alias cleanup="find . -name '*.DS_Store' -type f -ls -delete"
+
+# Shortcuts
+alias g="git"
+alias v="vim"
+
+# Shortcuts
+alias g="git"
+alias v="vim"
+
+# Empty the Trash on all mounted volumes and the main HDD
+alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; rm -rfv ~/.Trash"
+
+# Hide/show all desktop icons (useful when presenting)
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
+# Go up N directories
+up() {
+  str=""
+  count=0
+  while [ "$count" -lt "$1" ];
+  do
+    str=$str"../"
+    let count=count+1
+  done
+  cd $str
+}
